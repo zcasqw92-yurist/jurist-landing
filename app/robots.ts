786ab46df5c4adee7490b97ead73132fccd420f9
zcasqw92-yurist <zcasqw92@gmail.com>
2025-10-05 17:28:44 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,16 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  const base = (process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000').replace(/\/$/, '')
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: `${base}/sitemap.xml`,
     host: base,
   }
