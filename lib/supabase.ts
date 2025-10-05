8d07d05ef2ed6030397d5c53ee4a83aa5b9f9749
zcasqw92-yurist <zcasqw92@gmail.com>
2025-10-05 19:40:49 +0300

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE!;
+
+export const supabaseServer = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE, {
+  auth: { persistSession: false },
+});
